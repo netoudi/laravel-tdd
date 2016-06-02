c8e79f2fed2eeedfaf005ae2827d419765205305
netoudi <netoudi@rocketmail.com>
2016-06-02 17:26:56 -0300

    Criando usuário com senha criptografada.

--- codeuser/src/CodeUser/Repository/UserRepositoryEloquent.php
+++ codeuser/src/CodeUser/Repository/UserRepositoryEloquent.php
@@ -3,10 +3,18 @@
 namespace CodePress\CodeUser\Repository;
 
 use CodePress\CodeDatabase\AbstractRepository;
-use CodePress\CodeUser\Models\Tag;
+use CodePress\CodeUser\Models\User;
 
 class UserRepositoryEloquent extends AbstractRepository implements UserRepositoryInterface
 {
+    public function create(array  $data)
+    {
+        $password = $data['password'];
+        $data['password'] = bcrypt($password);
+        $result = parent::create($data);
+        return $result;
+    }
+
     public function model()
     {
         return User::class;
---
+++ codeuser/tests/CodeUser/Repository/UserRepositoryTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CodePress\CodeUser\Tests\Repository;
+
+use CodePress\CodeUser\Models\User;
+use CodePress\CodeUser\Repository\UserRepositoryEloquent;
+use CodePress\CodeUser\Tests\AbstractTestCase;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Validator;
+use Mockery as m;
+
+class UserRepositoryTest extends AbstractTestCase
+{
+    /**
+     * @var UserRepositoryEloquent
+     */
+    private $repository;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->migrate();
+        $this->repository = new UserRepositoryEloquent();
+    }
+
+    public function test_can_create_user()
+    {
+        $user = $this->repository->create([
+            'name' => 'Test',
+            'email' => 'test@test.com',
+            'password' => '123456'
+        ]);
+
+        $this->assertEquals('Test', $user->name);
+        $this->assertEquals('test@test.com', $user->email);
+        $this->assertTrue(Hash::check('123456', $user->password));
+    }
+}
