d7cafc07ded3b206dedb31d4acd64d4d473ff1d8
netoudi <netoudi@rocketmail.com>
2016-06-02 17:54:18 -0300

    Criando evento.

---
+++ codeuser/src/CodeUser/Event/UserCreatedEvent.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace CodePress\CodeUser\Event;
+
+class UserCreatedEvent
+{
+    private $user;
+    private $plainPassword;
+
+    /**
+     * UserCreatedEvent constructor.
+     * @param $user
+     * @param $plainPassword
+     */
+    public function __construct($user, $plainPassword)
+    {
+        $this->user = $user;
+        $this->plainPassword = $plainPassword;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    /**
+     * @param mixed $plainPassword
+     */
+    public function setPlainPassword($plainPassword)
+    {
+        $this->plainPassword = $plainPassword;
+        return $this;
+    }
+}
