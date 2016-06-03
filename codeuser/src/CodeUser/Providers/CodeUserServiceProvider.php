f38c99d0a02eba53821b8e4147a0748a58f297b2
netoudi <netoudi@rocketmail.com>
2016-06-02 22:09:00 -0300

    Criando template para email de registro.

--- codeuser/src/CodeUser/Providers/CodeUserServiceProvider.php
+++ codeuser/src/CodeUser/Providers/CodeUserServiceProvider.php
@@ -15,7 +15,8 @@ class CodeUserServiceProvider extends ServiceProvider
     {
         $this->publishes([__DIR__ . '/../../config/auth.php' => base_path('config/auth.php')], 'config');
         $this->publishes([__DIR__ . '/../../resources/migrations/' => base_path('database/migrations')], 'migrations');
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views/auth/', 'auth');
+        $this->publishes([__DIR__ . '/../../resources/views/auth' => base_path('resources/views/auth')], 'auth');
+        $this->publishes([__DIR__ . '/../../resources/views/email' => base_path('resources/views/email')], 'email');
         $this->loadViewsFrom(__DIR__ . '/../../resources/views/codeuser/', 'codeuser');
         require __DIR__ . '/../routes.php';
     }
---
+++ codeuser/src/resources/views/email/registration.blade.php
@@ -0,0 +1,19 @@
+<h3>CodePress</h3>
+
+<p>
+    Sua conta na plataforma foi criada.
+</p>
+
+<p>
+    Usuário: <strong>{{ $username }}</strong>
+</p>
+
+<p>
+    Senha: <strong>{{ $password }}</strong>
+</p>
+
+Acesse o sistema e troque sua senha.
+
+<hr>
+
+Obs.: Não responda este e-email, ele é gerado automaticamente.
