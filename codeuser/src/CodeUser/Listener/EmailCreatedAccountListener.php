98876ed6cddc9dbf83edd2713f64b698cb1cf818
netoudi <netoudi@rocketmail.com>
2016-06-02 21:27:31 -0300

    Criando EmailCreatedAccountListener - parte 2.

--- codeuser/src/CodeUser/Listener/EmailCreatedAccountListener.php
+++ codeuser/src/CodeUser/Listener/EmailCreatedAccountListener.php
@@ -6,7 +6,7 @@ use CodePress\CodeUser\Event\UserCreatedEvent;
 
 class EmailCreatedAccountListener
 {
-    public function handel(UserCreatedEvent $event)
+    public function handle(UserCreatedEvent $event)
     {
 
     }
--- codeuser/tests/CodeUser/Listener/EmailCreatedAccountListenerTest.php
+++ codeuser/tests/CodeUser/Listener/EmailCreatedAccountListenerTest.php
@@ -3,6 +3,7 @@
 namespace CodePress\CodeUser\Tests;
 
 use CodePress\CodeUser\Event\UserCreatedEvent;
+use CodePress\CodeUser\Listener\EmailCreatedAccountListener;
 use CodePress\CodeUser\Models\User;
 use Illuminate\Mail\Mailer;
 use Illuminate\Mail\Message;
@@ -19,9 +20,9 @@ class EmailCreatedAccountListenerTest extends AbstractTestCase
     public function test_can_trigger_handle()
     {
         $mockUser = m::mock(User::class);
-        $mockUser->name = 'Test';
-        $mockUser->email = 'test@test.com';
-        $mockUser->password = '123456';
+        $mockUser->shouldReceive('getAttribute')->with('name')->andReturn('Test');
+        $mockUser->shouldReceive('getAttribute')->with('email')->andReturn('test@test.com');
+        $mockUser->shouldReceive('getAttribute')->with('password')->andReturn('123456');
 
         $event = new UserCreatedEvent($mockUser, $mockUser->password);
 
@@ -29,7 +30,7 @@ class EmailCreatedAccountListenerTest extends AbstractTestCase
         $mockMailer->shouldReceive('send')
             ->with('email.registration', [
                 'username' => $mockUser->email,
-                'password' => $mockUser->password
+                'password' => $mockUser->password // plainPassword
             ], m::on(function (\Closure $closure) use ($mockUser) {
                 $mockMessage = m::mock(Message::class);
                 $mockMessage->shouldReceive('to')
@@ -38,6 +39,12 @@ class EmailCreatedAccountListenerTest extends AbstractTestCase
                 $mockMessage->shouldReceive('subject')
                     ->with("{$mockUser->name}, your account was created!");
                 $closure($mockMessage);
-            }));
+            }))
+            ->andReturn(1);
+
+        $listener = new EmailCreatedAccountListener($mockMailer);
+        $result = $listener->handle($event);
+
+        $this->assertEquals(1, $result);
     }
 }
