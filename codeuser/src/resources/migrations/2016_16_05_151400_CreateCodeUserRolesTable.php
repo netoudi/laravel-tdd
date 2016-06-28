fafe5f9d1faf046001f740614056e7fccd3a4eef
netoudi <netoudi@rocketmail.com>
2016-06-28 16:45:00 -0300

    Ajustes nas migrations do package CodeUser.

--- codeuser/src/resources/migrations/2016_16_05_151400_CreateCodeUserRolesTable.php
+++ codeuser/src/resources/migrations/2016_16_05_151400_CreateCodeUserRolesTable.php
@@ -13,9 +13,9 @@ class CreateCodeUserRolesTable extends Migration
     public function up()
     {
         Schema::create('codepress_users_roles', function (Blueprint $table) {
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('codepress_users');
-            $table->integer('role_id');
+            $table->integer('role_id')->unsigned();
             $table->foreign('role_id')->references('id')->on('codepress_roles');
         });
     }
@@ -27,6 +27,10 @@ class CreateCodeUserRolesTable extends Migration
      */
     public function down()
     {
+        Schema::table('codepress_users_roles', function (Blueprint $table) {
+            $table->dropForeign('codepress_users_roles_user_id_foreign');
+            $table->dropForeign('codepress_users_roles_role_id_foreign');
+        });
         Schema::drop('codepress_users_roles');
     }
 }
--- codeuser/src/resources/migrations/2016_16_05_151600_CreateCodePermissionsRolesTable.php
+++ codeuser/src/resources/migrations/2016_16_05_151600_CreateCodePermissionsRolesTable.php
@@ -13,9 +13,9 @@ class CreateCodePermissionsRolesTable extends Migration
     public function up()
     {
         Schema::create('codepress_permissions_roles', function (Blueprint $table) {
-            $table->integer('permission_id');
+            $table->integer('permission_id')->unsigned();
             $table->foreign('permission_id')->references('id')->on('codepress_permissions');
-            $table->integer('role_id');
+            $table->integer('role_id')->unsigned();
             $table->foreign('role_id')->references('id')->on('codepress_roles');
         });
     }
@@ -27,6 +27,10 @@ class CreateCodePermissionsRolesTable extends Migration
      */
     public function down()
     {
+        Schema::table('codepress_permissions_roles', function (Blueprint $table) {
+            $table->dropForeign('codepress_permissions_roles_permission_id_foreign');
+            $table->dropForeign('codepress_permissions_roles_role_id_foreign');
+        });
         Schema::drop('codepress_permissions_roles');
     }
 }
