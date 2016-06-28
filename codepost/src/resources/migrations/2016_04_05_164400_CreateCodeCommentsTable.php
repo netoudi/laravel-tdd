3c83e9054bb0b0ade3d4cef6bdf53691a3020bd4
netoudi <netoudi@rocketmail.com>
2016-06-28 16:34:30 -0300

    Ajustes nas migrations do package CodePost.

--- codepost/src/resources/migrations/2016_04_05_164400_CreateCodeCommentsTable.php
+++ codepost/src/resources/migrations/2016_04_05_164400_CreateCodeCommentsTable.php
@@ -3,14 +3,14 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateCodeCommentsTable
+class CreateCodeCommentsTable extends Migration
 {
     public function up()
     {
         Schema::create('codepress_comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('post_id');
-            $table->foreign('post_id')->references('id')->on('codepress_posts');
+            $table->integer('post_id')->unsigned();
+            $table->foreign('post_id')->references('id')->on('codepress_posts')->onDelete('cascade');
             $table->text('content');
             $table->timestamps();
         });
@@ -18,6 +18,9 @@ class CreateCodeCommentsTable
 
     public function down()
     {
+        Schema::table('codepress_comments', function (Blueprint $table) {
+            $table->dropForeign('codepress_comments_post_id_foreign');
+        });
         Schema::drop('codepress_comments');
     }
 }
